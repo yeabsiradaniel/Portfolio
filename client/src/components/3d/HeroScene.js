2878ddd3052e66d3ffc63e92402ce79e1f9bd0ec
yeabsiradaniel <yeabsirad9@gmail.com>
2026-06-13 17:23:04 +0300

    Remove unused useCallback import to fix CI build

--- client/src/components/3d/HeroScene.js
+++ client/src/components/3d/HeroScene.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useMemo, useCallback } from 'react';
+import React, { useRef, useEffect, useMemo } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import * as THREE from 'three';
 
